2dc4dc9d492decbbd3c386de6032b80e6793aeb8
MM4096 <85976396+MM4096@users.noreply.github.com>
2022-02-20 19:50:48 +1300

    picture slideshow done

--- script/slideshow.js
+++ script/slideshow.js
@@ -1,14 +1,16 @@
-var i = 1;
+var i = 2;
 $(document).ready(function() {
-    while (true) {
-        setTimeout(function() {
-            $(".slideshow").children().hide();
-            $(".slideshow").eq(i - 1).show();
+    $(".slideshow").children().hide();
+    $(".slideshow").children().eq(0).show();
+    const interval = setInterval(function() {
+
+            $(".slideshow").children().eq(i - 2).fadeTo(1000, 0).hide();
+            $(".slideshow").children().eq(i - 1).fadeTo(1000, 1).show();
             i++;
-            if (i = 6) {
+            if (i == 6) {
                 i = 1;
             }
-        }, 100);
+            console.log(i);
         
-    }
+    }, 5000)
 })
