95e2dec1c50fbf03b0066b1c608239e7239080bd
MM4096 <85976396+MM4096@users.noreply.github.com>
2022-02-23 12:22:03 +1300

    tests: a

--- script/cost.js
+++ script/cost.js
@@ -1,5 +1,6 @@
 //List of each item's info
-var peppermintCandy = {
+var item1 = {
+    code: "peppermintCandy",
     name: "Peppermint Candy",
     allergies: "<li>Mint</li>",
     itemDescription: "1 pack of peppermint candy contains: 10 peppermint candies. Looking for some snacks? Like the taste of mint? This is your #1 choice of candy!",
@@ -7,7 +8,8 @@ var peppermintCandy = {
     maxQty: 100,
 }
 
-var chocolate = {
+var item2 = {
+    code: "chocolate",
     name: "Chocolate",
     allergies: "<li>Milk</li><li>Soy</li><li>Nuts</li><li>Wheat</li>",
     itemDescription: "360g of Cadbury milk chocolate. For chocolate lovers!",
@@ -15,7 +17,8 @@ var chocolate = {
     maxQty: 50,
 }
 
-var lollipop = {
+var item3 = {
+    code: "lollipop",
     name: "Chupa Chups Lollipop",
     allergies: "<li></li>",
     itemDescription: "A single lollipop for a quick but long lasting lollipop",
@@ -23,10 +26,30 @@ var lollipop = {
     maxQty: 100,
 }
 
-var lollipop = {
-    name: "Chupa Chups Lollipop",
-    allergies: "<li></li>",
-    itemDescription: "A single lollipop for a quick but long lasting lollipop",
+var item4 = {
+    code: "allsortedLollies",
+    name: "Pascall's Allsorted lollies",
+    allergies: "<li>Wheat</li>",
+    itemDescription: "10 pack of Pascall's soft gummies",
     cost: 0.5,
     maxQty: 100,
 }
+
+var item5 = {
+    code: "candyCorn",
+    name: "Candy Corn",
+    allergies: "<li></li>",
+    itemDescription: "20 pack of Candy Corn, a popular Halloween candy!",
+    cost: 1,
+    maxQty: 100,
+}
+
+var item6 = {
+    code: "sourGummyWorms",
+    name: "Trolli Sour Gummy Worms",
+    allergies: "<li></li>",
+    itemDescription: "25 pack of Sour Gummy Worms",
+    cost: 1,
+    maxQty: 100,
+}
+
--- script/fillIn.js
+++ script/fillIn.js
@@ -8,7 +8,16 @@ $(document).ready(function() {
     if (requestedItem == null || requestedItem == undefined || requestedItem == "") {
         window.alert("You are viewing the sample page");
     } else {
-
+        for (let i = 1; i <= 6; i++) {
+            let objectName = "item" + i;
+            if (objectName.code == requestedItem) {
+                var requestedObject = objectName
+            }
+        }
+        $("h1").first().text(objectName.name);
+        $("ul").first().html(objectName.allergies);
+        $("#description").text(objectName.itemDescription);
+        $(".cost").first().text(objectName.cost);
     }
 })
 
